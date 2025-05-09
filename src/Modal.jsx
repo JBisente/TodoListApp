a48ac13bcd0292c0ae3eb98b98f5c76c21b5a5a4
MGChristian <132035945+MGChristian@users.noreply.github.com>
2025-05-09 18:56:46 +0800

    added add task functionality

---
+++ src/Modal.jsx
@@ -0,0 +1,60 @@
+import { X } from "lucide-react";
+import { useState } from "react";
+
+function Modal({ open, onClose, addTask }) {
+  const [task, setTask] = useState({
+    time: "",
+    task: "",
+  });
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    addTask(task);
+    setTask({ time: "", task: "" });
+    onClose();
+  }
+
+  return (
+    // MODAL
+    <div
+      className={`fixed flex h-full w-full items-center justify-center bg-gray-500/30 ${open ? "visible" : "invisible"}`}
+    >
+      <div className="h-fit w-full max-w-sm rounded-lg bg-gray-100 p-4">
+        <form className="flex flex-col gap-4">
+          <div className="flex justify-between">
+            <p className="font-bold">Add Task</p>
+            <X onClick={onClose} className="cursor-pointer" />
+          </div>
+          <div className="flex flex-col">
+            <label>Name</label>
+            <input
+              onChange={(e) => setTask({ ...task, task: e.target.value })}
+              value={task.name}
+              type="text"
+              className="rounded-lg border border-gray-400 bg-transparent px-2 py-4"
+            />
+          </div>
+          <div className="flex flex-col">
+            <label>Time</label>
+            <input
+              onChange={(e) => setTask({ ...task, time: e.target.value })}
+              value={task.time}
+              type="time"
+              className="rounded-lg border border-gray-400 bg-transparent px-2 py-4"
+            />
+          </div>
+          <div>
+            <button
+              onClick={(e) => handleSubmit(e)}
+              className="float-right rounded-full bg-violet-800/50 px-8 py-2 text-white"
+            >
+              Add
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+}
+
+export default Modal;
--- src/TodoList.jsx
+++ src/TodoList.jsx
@@ -1,29 +1,98 @@
-import { Plus } from 'lucide-react';
+import { Plus, Trash } from "lucide-react";
+import { useState } from "react";
+import Modal from "./Modal";
 
 const TodoList = () => {
   const getGreeting = () => {
     const hour = new Date().getHours();
-    if (hour < 12) return 'Good morning!';
-    if (hour < 18) return 'Good afternoon!';
-    return 'Good evening!';
+    if (hour < 12) return "Good morning!";
+    if (hour < 18) return "Good afternoon!";
+    return "Good evening!";
   };
 
-  return (
-    <div className="min-h-screen bg-gray-100 p-4">
-      <div className="w-full max-w-sm mx-auto">
-        <div className="flex justify-between items-center mb-6">
-          <div>
-            <h1 className="text-2xl font-bold text-gray-800">{getGreeting()}</h1>
-            <p className="text-md text-gray-500">Start scheduling your daily task.</p>
-          </div>
-          <button className="w-12 h-12 rounded-full bg-violet-800/50 text-white flex items-center justify-center">
-            <Plus />
-          </button>
-        </div>
+  // TASKS
+  const [tasks, setTasks] = useState([
+    { time: "08:00", task: "Go to church" },
+    { time: "09:00", task: "Cook for Family" },
+  ]);
 
-        <h2 className="text-xl font-semibold text-gray-800 mb-4">Today's tasks</h2>
+  function addTask(newTask) {
+    setTasks((t) => [...t, newTask]);
+  }
+  function deleteTask(index) {}
+
+  // MODAL
+  const [open, setOpen] = useState(false);
+  return (
+    <>
+      <Modal open={open} onClose={() => setOpen(false)} addTask={addTask} />
+      <div className="min-h-screen bg-gray-100">
+        <div className="mx-auto w-full max-w-sm rounded-lg p-4 shadow-lg">
+          <div className="mb-6 flex items-center justify-between">
+            <div>
+              <h1 className="text-2xl font-bold text-gray-800">
+                {getGreeting()}
+              </h1>
+              <p className="text-md text-gray-500">
+                Start scheduling your daily task.
+              </p>
+            </div>
+            <button
+              onClick={() => setOpen(true)}
+              className="flex h-12 w-12 items-center justify-center rounded-full bg-violet-800/50 text-white"
+            >
+              <Plus />
+            </button>
+          </div>
+
+          <h2 className="mb-4 text-xl font-semibold text-gray-800">
+            Today's tasks
+          </h2>
+          <ol>
+            {tasks.map((task, index) => {
+              return (
+                <li
+                  key={index}
+                  className="my-2 flex items-center justify-between rounded-lg border border-gray-400 p-2"
+                >
+                  <div className="flex gap-4">
+                    <input type="checkbox" />
+                    <div className="flex flex-col gap-2">
+                      <p className="text-xs text-gray-600">{task.time}</p>
+                      <p>{task.task}</p>
+                    </div>
+                  </div>
+                  <div>
+                    <Trash size={16} onClick={() => deleteTask(index)} />
+                  </div>
+                </li>
+              );
+            })}
+          </ol>
+          <h2 className="mb-4 text-xl font-semibold text-gray-800">
+            Completed Tasks
+          </h2>
+          <ol>
+            {tasks.map((task, index) => {
+              return (
+                <li className="my-2 flex items-center justify-between rounded-lg border border-gray-400 p-2">
+                  <div className="flex gap-4">
+                    <input type="checkbox" />
+                    <div className="flex flex-col gap-2">
+                      <p className="text-xs text-gray-600">{task.time}</p>
+                      <p>{task.task}</p>
+                    </div>
+                  </div>
+                  <div>
+                    <Trash size={16} />
+                  </div>
+                </li>
+              );
+            })}
+          </ol>
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
